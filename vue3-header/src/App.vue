d18828a6dad3be3d3f6b7071c8b0c49cd1c0dff4
GabrieleMajocchi <gabriele.majocchi@gmail.com>
2023-06-15 12:45:35 +0200

    Created scaffolding with appheader showing some links

---
+++ vue3-header/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import AppHeader from './components/AppHeader.vue'
+</script>
+
+<template>
+
+    <AppHeader/>
+    
+</template>
+
+<style lang="scss">
+@import url(./styles/general.scss);
+
+</style>
---
+++ vue3-header/src/components/AppHeader.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+    
+}
+</script>
+
+<template>
+    <div>
+        <a href="#">Link 1</a>
+        <a href="#">Link 1</a>
+        <a href="#">Link 1</a>
+        <a href="#">Link 1</a>
+    </div>
+</template>
+
+<style lang="scss">
+@use '../styles/partials/mixins' as *;
+@use '../styles/partials/variables' as *;
+
+div{
+    a{
+        text-decoration: none;
+        margin: 1rem 0;
+        color: black;
+    }
+}
+
+</style>
